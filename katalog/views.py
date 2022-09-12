938aa2761f07ed661150b361b6f73203fdfb7c77
Thalia Fortuna <thaliafortuna8@gmail.com>
2022-09-12 22:10:37 +0700

    views.py tweaks

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+
+
+# Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_katalog,
+        'nama': 'Thalia',
+        'id': '2106751890'
+    }
+    return render(request, "katalog.html", context)
