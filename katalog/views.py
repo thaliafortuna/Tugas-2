ee13164cc93dee01bc077b5a533cc6fde8ca4344
Thalia Fortuna <thaliafortuna8@gmail.com>
2022-09-14 16:45:08 +0700

    organize

--- katalog/views.py
+++ katalog/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from katalog.models import CatalogItem
 
-# TODO: Create your views here.
 def show_katalog(request):
     data_barang_katalog = CatalogItem.objects.all()
     context = {
