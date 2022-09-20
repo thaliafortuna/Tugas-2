29e25a207cb1e128a3d7cdc271aa7e750baee26e
Thalia Fortuna <thaliafortuna8@gmail.com>
2022-09-20 22:50:25 +0700

    Tugas 3

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class MyWatchList(models.Model):
-    watched = models.TextField()
+    watched = models.CharField(max_length=140, null=True)
     title = models.TextField()
     rating = models.IntegerField()
-    release_date = models.CharField(max_length=255)
-    review = models.TextField()
+    release_date = models.CharField(max_length=140, null=True)
+    review = models.CharField(max_length=140, null=True)
