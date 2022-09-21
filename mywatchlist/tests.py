69f55c930e74a684004b40529bafb9a65878c882
Thalia Fortuna <thaliafortuna8@gmail.com>
2022-09-21 22:40:11 +0700

    tests.py

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import resolve
 
 # Create your tests here.
+
+class AppTest(TestCase):
+    def test_html(self):
+        response = Client().get('https://aplikasidjango.herokuapp.com/mywatchlist/html/')
+        self.assertEqual(response.status_code,200)
+    def test_json(self):
+        response = Client().get('https://aplikasidjango.herokuapp.com/mywatchlist/json/')
+        self.assertEqual(response.status_code,200)
+    def test_xml(self):
+        response = Client().get('https://aplikasidjango.herokuapp.com/mywatchlist/xml/')
+        self.assertEqual(response.status_code,200)
