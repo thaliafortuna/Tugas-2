356f6cacb6bf49b1b2d853aef07ab46e270940ba
Thalia Fortuna <thaliafortuna8@gmail.com>
2022-09-21 18:01:56 +0700

    models char length updated

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class MyWatchList(models.Model):
-    title = models.CharField(max_length=140, null=True)
-    watched = models.CharField(max_length=140, null=True)
-    rating = models.CharField(max_length=140, null=True)
-    release_date = models.CharField(max_length=140, null=True)
-    review = models.CharField(max_length=140, null=True)
+    title = models.CharField(max_length=300, null=True)
+    watched = models.CharField(max_length=300, null=True)
+    rating = models.CharField(max_length=300, null=True)
+    release_date = models.CharField(max_length=300, null=True)
+    review = models.CharField(max_length=300, null=True)
