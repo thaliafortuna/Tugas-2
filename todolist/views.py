8b9071d4ffaa106a96d47231e9adda53aa396eb3
Thalia Fortuna <thaliafortuna8@gmail.com>
2022-09-28 21:40:48 +0700

    Tugas 4 todolist

--- todolist/views.py
+++ todolist/views.py
@@ -42,7 +42,6 @@ def login_user(request):
 def logout_user(request):
     logout(request)
     response = HttpResponseRedirect(reverse('todolist:login'))
-
     return response
 
 @login_required(login_url='/todolist/login/')
@@ -73,40 +72,3 @@ def create_todolist(request):
     }
     return render(request, "create-task.html", context)
 
-
-
-
-
-
-
-# class TaskList(ListView):
-#     model = ListToDo
-#     context_object_name= "tasks"
-#     template_name= "todolist.html"
-    
-
-# class TaskCreate(CreateView):
-#     model = ListToDo
-#     template_name= "create-task.html"
-#     fields = '__all__'
-#     success_url = reverse_lazy('show_todolist')
-
-
-# # formerly get_name
-# def create_task(request): 
-#     # if this is a POST request we need to process the form data
-#     if request.method == 'POST':
-#         # create a form instance and populate it with data from the request:
-#         form = TaskForm(request.POST)
-#         # check whether it's valid:
-#         if form.is_valid():
-#             form.save()
-#             messages.success(request, 'Task is successfully made!')
-#             return redirect('todolist')
-            
-#     # if a GET (or any other method) we'll create a blank form
-#     else:
-#         form = TaskForm()
-
-#     context = {'form':form}
-#     return render(request, 'create-task.html', {'form': form})
